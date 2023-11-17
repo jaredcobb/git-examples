f82299073c57c96c96b9d6fcdb3f85fb5d8516fa
Jared Cobb <github@jaredcobb.com>
2023-11-17 10:56:33 -0500

    fixes

--- commits.sh
+++ commits.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-# Set locale for tr
-LC_ALL=C
-
 # Create a new file named example.txt
 echo "Initial content" > example.txt
 
 # Function to generate a random string
 generate_random_string() {
-    cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${1:-32} | head -n 1
+    head /dev/urandom | tr -dc A-Za-z0-9 | head -c 10
 }
 
 # Repeat 20 times
 for i in {1..20}
 do
     # Generate random content
-    random_content=$(generate_random_string 10)
+    random_content=$(generate_random_string)
 
     # Determine the number of lines in the file
     total_lines=$(wc -l < example.txt)
@@ -25,7 +22,8 @@ do
     line_number=$(($RANDOM % total_lines + 1))
 
     # Insert the random content into a random location in the file
-    sed -i "${line_number}i$random_content" example.txt
+    sed -i '' "${line_number}i\\
+    $random_content" example.txt
 
     # Add and commit the change
     git add example.txt
